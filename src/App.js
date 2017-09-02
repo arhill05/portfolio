d9a4c0f7880c243676b9dff950760f42555b546e
Drew Hill <mdude2314@gmail.com>
2017-09-02 18:01:15 -0400

    start portfolio section styling

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
-import React, {Component} from "react";
+import React, { Component } from "react";
 import Header from "./components/Header";
 import "./bootstrap.min.css";
 import "../node_modules/font-awesome/css/font-awesome.min.css";
 import "./App.css";
 
+const socratesImg = require('./img/socrates.png');
+
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Header/>
+        <Header />
         <div className="content">
           <div className="container">
             <div className="card about">
@@ -32,28 +34,40 @@ class App extends Component {
               </div>
             </div>
           </div>
-          <div className="shadow">
+          <div className="shadow section-heading-shadow">
             <div className="portfolio-section-heading">
-              <p>A H</p>
+              <p>Portfolio</p>
             </div>
           </div>
-          <div className="container">
-            <div className="portfolio">
-              <div className="portfolio-item item-1">
-                <div className="portfolio-heading">
-                  <h2>Socrates</h2>
-                  <div className="action-buttons">
-                    <button>
-                      <i className="fa fa-github icon" aria-hidden="true"></i>GitHub</button>
-                    <button>
-                      <span className="icon">www</span>Live Site</button>
-                  </div>
+          <div className="container portfolio">
+            <div className="portfolio-item item-1">
+              <div className="portfolio-heading">
+                <h2>Socrates</h2>
+                <div className="action-buttons">
+                  <a href="https://github.com/arhill05/socrates-rewrite">
+                    <i className="fa fa-github icon" aria-hidden="true"></i>GitHub</a>
+                  <a href="https://socratesapp.co">
+                    <span className="icon">www</span>Live Site</a>
                 </div>
               </div>
-              <div className="portfolio-item item-2"></div>
-              <div className="portfolio-item item-3"></div>
-              <div className="portfolio-item item-4"></div>
+              <div className="portfolio-item-badges">
+                <div className="badge">AngularJS</div>
+                <div className="badge">HTML5</div>
+                <div className="badge">Firebase</div>
+                <div className="badge">LESS</div>
+              </div>
+              <p className="portfolio-item-descripion">
+              <img src={socratesImg} />
+                Socrates is an anonymous Q&A web app. Users can create sessions, ask questions in sessions, upvote questions they agree with,
+                    and see other user's questions, all in real time. The idea is students who are afraid to ask questions in a classroom can
+                    ask questions without fear of judgement by their peers, or Q&A sessions held at conferences can allow the audience to ask questions
+                    and see the most poular questions without ever having to hand the microphone to an audience member for them to tell their life's story
+                    in the mean time.
+                  </p>
             </div>
+            <div className="portfolio-item item-2"></div>
+            <div className="portfolio-item item-3"></div>
+            <div className="portfolio-item item-4"></div>
           </div>
         </div>
       </div>
